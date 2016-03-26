318e4fb84080049dccaf3b3c12af70697c9c5ede
JIHAN LEE <globalrt@naver.com>
2016-03-26 16:06:49 +0900

    Initial Commit

---
+++ 0-1Knapsack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  0-1Knapsack
+//
+//  Created by JIBA on 2016. 3. 26..
+//  Copyright © 2016년 JIBA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
